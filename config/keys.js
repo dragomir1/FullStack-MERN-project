c99da0d9cca056990dabcb5918223751393ad91a
Michael Dragomir <michaeldragomir@mac.com>
2018-11-01 09:15:39 -0700

    additions

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://mike11:mike11@ds129946.mlab.com:29946/socialmediamernproject'
+  mongoURI: 'mongodb://mike11:mike11@ds129946.mlab.com:29946/socialmediamernproject',
+  secretOrKey: "secret"
 }
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcryptjs = require('bcryptjs');
+const passport = require('passport');
+
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -18,15 +23,18 @@ mongoose.connection
   .once('open', () => console.log('Connected to MongoLab instance.'))
   .on('error', error => console.log('Error connecting to MongoLab:', error));
 
-app.use(bodyParser.json());
+
 
 // routes
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
 
+// passport
+app.use(passport.initialize());
 
-app.get('/', (req, res) => res.send('hello world'));
+// passport config
+require('./config/passport')(passport);
 
 
 // port
