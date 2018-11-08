d9ae298532fc4eb417e0da85911fab163a04bcbe
Michael Dragomir <michaeldragomir@mac.com>
2018-11-08 08:05:16 -0800

    created layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import Navbar from './components/Layout/Navbar';
+import Landing from './components/Layout/Landing';
+import Footer from './components/Layout/Footer';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 // import logo from './assets/images/showcase.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1>React APP</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+
+
+            </div>
+            <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ client/src/components/Auth/Register.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import axios from 'axios';
+
+class Register extends Component {
+
+constructor() {
+  super();
+  this.state = {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      errors: {}
+  }
+  this.onChange = this.onChange.bind(this);
+  this.onSubmit = this.onSubmit.bind(this);
+}
+
+onChange = (event) => {
+  this.setState({[event.target.name]: event.target.value})
+}
+
+onSubmit = (event) => {
+  event.preventDefault();
+
+  const newUser = {
+    name: this.state.name,
+    email: this.state.email,
+    password: this.state.password,
+    passwordConfirm: this.state.passwordConfirm,
+  }
+  axios.post('/api/users/register', newUser)
+    .then(res => console.log(res.data))
+    .catch(err => this.setState({errors: err.response.data}))
+}
+
+// const { errors } = this.state; the { } allow you to pull errors out of this.state.  so you don't need to assign it directly => const errors = this.state.errors
+  render() {
+
+    const { errors } = this.state;
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnector account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input type="text"
+                  className={classnames("form-control form-control-lg", {
+                    'is-invalid' : errors.name })}
+                  placeholder="Name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
+                  />
+                  {errors.name && (<div className="invalid-feedback">{errors.name}</div>) }
+                </div>
+                <div className="form-group">
+                  <input type="email"
+                  className={classnames("form-control form-control-lg", {
+                    'is-invalid' : errors.email })}
+                  placeholder="Email Address"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  />
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>) }
+                </div>
+                <div className="form-group">
+                  <input type="password"
+                  className={classnames("form-control form-control-lg", {
+                    'is-invalid' : errors.password })}
+                  placeholder="Password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  />
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>) }
+                </div>
+                <div className="form-group">
+                  <input type="password"
+                  className={classnames("form-control form-control-lg", {
+                    'is-invalid' : errors.passwordConfirm})}
+                  placeholder="Confirm Password"
+                  name="passwordConfirm"
+                  value={this.state.passwordConfirm}
+                  onChange={this.onChange}
+                  />
+                  {errors.passwordConfirm && (<div className="invalid-feedback">{errors.passwordConfirm}</div>) }
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register;
